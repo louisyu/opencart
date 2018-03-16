8f10c3726b701fbf75aef316be3b3684f0104487
louisyu <tsungtseyu@gmail.com>
2018-03-16 08:29:23 +0800

    add on zh_TW language

---
+++ upload/admin/language/zh-TW/marketplace/cron.php
@@ -0,0 +1,25 @@
+<?php
+// Heading
+$_['heading_title']     = '工作排程';
+
+// Text
+$_['text_success']      = '成功: 您已修改工作排程!';
+$_['text_list']         = '事件清單';
+$_['text_cron']         = '工作排程是定期執行的計劃任務';
+$_['text_action']       = '操作';
+$_['text_hour']         = '時';
+$_['text_day']          = '日';
+$_['text_month']        = '月';
+
+// Column
+$_['column_code']       = '工作排程 Code';
+$_['column_cycle']      = '週期';
+$_['column_cycle']      = '週期';
+
+
+$_['column_status']     = '狀態';
+$_['column_sort_order'] = '排序';
+$_['column_action']     = '操作';
+
+// Error
+$_['error_permission']  = '警告: 您沒有權限修改工作排程!';
---
+++ upload/admin/language/zh-TW/tool/upgrade.php
@@ -0,0 +1,51 @@
+<?php
+// Heading
+$_['heading_title']      = '更新';
+
+// Text
+$_['text_success']       = '成功: 您已修改上傳檔案!';
+$_['text_system']        = '更新系統';
+$_['text_version']       = '最新版本';
+$_['text_compatibility'] = '檢查相容性';
+
+
+$_['text_progress']      = '更新進度';
+$_['text_log']           = '更新紀錄';
+$_['text_compare']       = '檔案比較';
+
+
+$_['text_log']           = '更新紀錄 Log';
+
+$_['text_current']       = '下載最新版本';
+$_['text_download']      = '下載最新版本';
+
+// Column
+$_['column_name']        = 'Extension 名稱';
+$_['column_latest']      = '最新版本';
+$_['column_status']      = '狀態';
+
+// Entry
+$_['entry_name']         = '上傳名稱';
+$_['entry_filename']     = '檔案名稱';
+$_['entry_date_added']   = '新增日期';
+$_['entry_name']         = '上傳名稱';
+$_['entry_patch']        = 'Patch';
+
+// Help
+$_['help_patch']         = 'patch 說明';
+
+// Button
+$_['button_skip']        = '跳過';
+$_['button_patch']       = 'Patch';
+$_['button_help']        = '說明';
+
+// Tab
+$_['tab_version']        = '1. 最新版本';
+$_['tab_compatibility']  = '2. 檢查相容性';
+$_['tab_upgrade']        = '3. 系統更新';
+$_['tab_finish']         = '4. 完成';
+
+// Error
+$_['error_permission']   = '警告: 您沒有權限進行更新!';
+$_['error_version']      = '您的 OpenCart 版本並不是最新的! 目前最新的版本是 <strong>%s</strong>';
+$_['error_connection']   = '警告: 無法連接到 upgrade server!';
