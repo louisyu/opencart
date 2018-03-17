f6e912d81595a6f45d6da88c9380833863bdb357
louisyu <tsungtseyu@gmail.com>
2018-03-17 19:34:40 +0800

    remove install directory

--- upload/install/language/zh-TW/common/footer.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Text
-$_['text_project']       = '專案首頁';
-$_['text_documentation'] = '說明文件';
-$_['text_support']       = '支援論壇';
-$_['text_footer']        = 'Copyright © 2014 OpenCart - All rights reserved';
